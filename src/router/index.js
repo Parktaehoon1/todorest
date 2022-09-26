5bf708f6288258387e4d3a6475a38f855b266495
Parktaehoon1 <92parkth@gmail.com>
2022-09-26 13:06:30 +0900

    Pagination 진행

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-const routes = [];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes: [],
 });
 
 export default router;
